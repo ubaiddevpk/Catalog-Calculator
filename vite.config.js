5dabb6c14855c1e46506be8a40277c675dfcd20e
Obaid ullah <obaidullah.developer@gmail.com>
2026-01-08 19:27:49 +0500

    fix deployment error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",       // ✅ Important for Vercel deployment
-  server: {
-    port: 3000,
-  },
+  base: "/", // important so Vercel loads JS/CSS correctly
 });
